ea3b46159229d3ba5334e89880b80edf403b335d
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2021-02-25 22:05:48 -0600

    Created unit tests
    Created unit tests for showing/hiding event details and specifying the number of events (along with other tests done during the throughout the reading).

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <EventList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <NumberOfEvents />
+        <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
 
-    test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
+    let AppWrapper;
+
+    beforeAll(() => {
+        AppWrapper = shallow(<App />);
+    });
+
+    test('render list of events component', () => {
         expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
+
+    test('render city search component', () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+    });
+
+    test('render number of events component', () => {
+        expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
     });
 
 });
